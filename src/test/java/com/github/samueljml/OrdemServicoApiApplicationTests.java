8342a26de9b77fe7dcc31d2dfc47954c77ebaa9e
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-04-26 21:57:48 -0300

    Primeiro commit

---
+++ src/test/java/com/github/samueljml/OrdemServicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.samueljml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
