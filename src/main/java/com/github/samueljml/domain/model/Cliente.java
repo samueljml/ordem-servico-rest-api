a015fd0e70f79ba1308ab0be6a68885fc08651a5
= <samuel.andrade@fatec.sp.gov.br>
2021-04-28 18:30:06 -0300

    Adicionando anotações JPA no Cliente

--- src/main/java/com/github/samueljml/domain/model/Cliente.java
+++ src/main/java/com/github/samueljml/domain/model/Cliente.java
@@ -1,8 +1,17 @@
 package com.github.samueljml.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String telefone;
