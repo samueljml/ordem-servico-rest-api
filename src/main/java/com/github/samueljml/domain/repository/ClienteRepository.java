d9b962206694b96470fe71cd210861d4b061a538
= <samuel.andrade@fatec.sp.gov.br>
2021-04-28 18:30:40 -0300

    Adicionando Repositório para Cliente

---
+++ src/main/java/com/github/samueljml/domain/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.github.samueljml.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.samueljml.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	
+}
