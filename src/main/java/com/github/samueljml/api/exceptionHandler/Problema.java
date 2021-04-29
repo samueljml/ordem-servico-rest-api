7e8b91e702f871cd831b256fa8a69bceb1fdca17
= <samuel.andrade@fatec.sp.gov.br>
2021-04-28 23:06:15 -0300

    Tratando Exception: campos inválidos
    Retornando mensagem mais adequada para o consumidor da API quando for violada alguma regra de validação dos campos.

---
+++ src/main/java/com/github/samueljml/api/exceptionHandler/Problema.java
@@ -0,0 +1,60 @@
+package com.github.samueljml.api.exceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Problema {
+	private int status;
+	private String titulo;
+	private LocalDateTime dataHora;
+	private List<Campo> campos;
+	
+	public static class Campo {
+		private String nome;
+		private String mensagem;
+				
+		Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getMensagem() {
+			return mensagem;
+		}
+		public void setMensagem(String mensagem) {
+			this.mensagem = mensagem;
+		}
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public List<Campo> getCampos() {
+		return campos;
+	}
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+}
