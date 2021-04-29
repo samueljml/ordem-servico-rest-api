a43e55b9ec1fe759e5fbde70f051aedc4e9656b5
= <samuel.andrade@fatec.sp.gov.br>
2021-04-28 22:27:11 -0300

    Validando dados do Cliente com as anotações do Bean Validation

--- src/main/java/com/github/samueljml/domain/model/Cliente.java
+++ src/main/java/com/github/samueljml/domain/model/Cliente.java
@@ -4,6 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
 @Entity
 public class Cliente {
@@ -12,8 +17,17 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Size(max = 225)
+	@Email
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 
 	public Long getId() {
