04ce66196aab3706826bf970684709f71a9f78de
= <samuel.andrade@fatec.sp.gov.br>
2021-04-28 22:06:32 -0300

    Adicionando ClienteController e endpoints

---
+++ src/main/java/com/github/samueljml/api/controller/ClienteController.java
@@ -0,0 +1,71 @@
+package com.github.samueljml.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.samueljml.domain.model.Cliente;
+import com.github.samueljml.domain.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+	@Autowired
+	private ClienteRepository clienteRepo;
+
+	@GetMapping
+	public List<Cliente> listar() {
+		return clienteRepo.findAll();
+	}
+
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepo.findById(clienteId);
+
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepo.save(cliente);
+	}
+
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+		if (!clienteRepo.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		clienteRepo.save(cliente);
+
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if (!clienteRepo.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepo.deleteById(clienteId);
+
+		return ResponseEntity.noContent().build();
+	}
+
+}
