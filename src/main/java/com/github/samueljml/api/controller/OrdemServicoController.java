9ab1e29eeddecec01401db58e8f40dddb2bc8029
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-04-29 17:58:05 -0300

    POST para OrdemServico

--- src/main/java/com/github/samueljml/api/controller/OrdemServicoController.java
+++ src/main/java/com/github/samueljml/api/controller/OrdemServicoController.java
@@ -1,9 +1,14 @@
 package com.github.samueljml.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.samueljml.domain.model.OrdemServico;
 import com.github.samueljml.domain.model.service.OrdemServicoService;
 import com.github.samueljml.domain.repository.OrdemServicoRepository;
 
@@ -16,4 +21,10 @@ public class OrdemServicoController {
 	
 	@Autowired
 	private OrdemServicoService ordemServicoService;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public OrdemServico criar(@RequestBody OrdemServico ordemServico) {
+		return ordemServicoService.criar(ordemServico);
+	}
 }
--- src/main/java/com/github/samueljml/domain/model/service/OrdemServicoService.java
+++ src/main/java/com/github/samueljml/domain/model/service/OrdemServicoService.java
@@ -1,14 +1,35 @@
 package com.github.samueljml.domain.model.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.samueljml.domain.exception.NegocioException;
+import com.github.samueljml.domain.model.Cliente;
+import com.github.samueljml.domain.model.OrdemServico;
+import com.github.samueljml.domain.model.StatusOrdemServico;
+import com.github.samueljml.domain.repository.ClienteRepository;
 import com.github.samueljml.domain.repository.OrdemServicoRepository;
 
 @Service
 public class OrdemServicoService {
 	
 	@Autowired
-	private OrdemServicoRepository OndemServicoRepo;
+	private OrdemServicoRepository OrdemServicoRepo;
+	
+	@Autowired
+	private ClienteRepository clienteRepo;
+	
+	public OrdemServico criar(OrdemServico ordemServico) {
+		Cliente cliente = clienteRepo.findById(ordemServico.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+		
+		ordemServico.setCliente(cliente);
+		ordemServico.setDataAbertura(LocalDateTime.now());
+		ordemServico.setStatus(StatusOrdemServico.ABERTA);
+		return OrdemServicoRepo.save(ordemServico);
+	}
 	
 }
