58840ee12b37577785de0931b401b5bef4744e3a
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-04-30 13:44:06 -0300

    Criando um Repesentation Model para OrdemServico
    Evitando utilizar a mesma classe para representar o modelo de domínio e o modelo de representação, facilitando a manutenção do código e definindo quais atributos podem ser expostos ao usuário.

---
+++ src/main/java/com/github/samueljml/api/model/OrdemServicoModel.java
@@ -0,0 +1,60 @@
+package com.github.samueljml.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.github.samueljml.domain.model.StatusOrdemServico;
+
+public class OrdemServicoModel {
+
+	private Long id;
+	private String nomeCliente;
+	private String drescicao;
+	private BigDecimal preco;
+	private StatusOrdemServico status;
+	private OffsetDateTime dataAbertura;
+	private OffsetDateTime dataFinalizacao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getDrescicao() {
+		return drescicao;
+	}
+	public void setDrescicao(String drescicao) {
+		this.drescicao = drescicao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	public StatusOrdemServico getStatus() {
+		return status;
+	}
+	public void setStatus(StatusOrdemServico status) {
+		this.status = status;
+	}
+	public OffsetDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(OffsetDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	public OffsetDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+}
