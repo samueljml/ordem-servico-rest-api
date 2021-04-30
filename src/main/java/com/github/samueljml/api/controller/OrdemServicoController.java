5f462e06c979e3abc7f4f62f183a54e2bf730717
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-04-30 13:04:01 -0300

    GET para OrdemServico

--- src/main/java/com/github/samueljml/api/controller/OrdemServicoController.java
+++ src/main/java/com/github/samueljml/api/controller/OrdemServicoController.java
@@ -1,9 +1,15 @@
 package com.github.samueljml.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +34,21 @@ public class OrdemServicoController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
 		return ordemServicoService.criar(ordemServico);
+	}
+	
+	@GetMapping
+	public List<OrdemServico> listar() {
+		return ordemServicoRepo.findAll();
+	}
+	
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId) {
+		Optional<OrdemServico> ordemServico = ordemServicoRepo.findById(ordemServicoId);
+		
+		if(ordemServico.isPresent()) {
+			return ResponseEntity.ok(ordemServico.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
