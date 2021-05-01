834c87972b06052467f4a7cfb344fd2e56bc86d9
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-04-30 22:34:31 -0300

    POST para comentário

--- src/main/java/com/github/samueljml/domain/model/service/OrdemServicoService.java
+++ src/main/java/com/github/samueljml/domain/model/service/OrdemServicoService.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.github.samueljml.domain.exception.NegocioException;
 import com.github.samueljml.domain.model.Cliente;
+import com.github.samueljml.domain.model.Comentario;
 import com.github.samueljml.domain.model.OrdemServico;
 import com.github.samueljml.domain.model.StatusOrdemServico;
 import com.github.samueljml.domain.repository.ClienteRepository;
+import com.github.samueljml.domain.repository.ComentarioRepository;
 import com.github.samueljml.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -21,6 +23,9 @@ public class OrdemServicoService {
 	@Autowired
 	private ClienteRepository clienteRepo;
 	
+	@Autowired 
+	private ComentarioRepository comentarioRepo;
+	
 	public OrdemServico criar(OrdemServico ordemServico) {
 		Cliente cliente = clienteRepo.findById(ordemServico.getCliente().getId())
 				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
@@ -29,6 +34,18 @@ public class OrdemServicoService {
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		return OrdemServicoRepo.save(ordemServico);
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		OrdemServico ordemServico = OrdemServicoRepo.findById(ordemServicoId)
+				.orElseThrow(() -> new NegocioException("Ordem de serviço não encontrada"));
+		
+		Comentario comentario = new Comentario();
+		comentario.setDataEnvio(OffsetDateTime.now());
+		comentario.setDescricao(descricao);
+		comentario.setOrdemServico(ordemServico);
+	
+		return comentarioRepo.save(comentario);
 	}
 	
 }
